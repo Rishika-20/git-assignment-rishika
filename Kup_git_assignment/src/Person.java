3cb3a3d2a500002184d9a86113209a3c8f337d1c
Rishika0101 <rishika.kumari@knoldus.com>
2023-03-30 23:34:45 +0530

    typo mistake in person class commit

--- Kup_git_assignment/src/Person.java
+++ Kup_git_assignment/src/Person.java
@@ -1,24 +1,19 @@
 public class Person {
-
     String name;
-    Integer age;
-
+    // typo mistake.
+    Intege age;
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Integer getAge() {
         return age;
     }
-
     public void setAge(Integer age) {
         this.age = age;
     }
-
     public Person(String name, Integer age) {
         this.name = name;
         this.age = age;
@@ -27,5 +22,4 @@ public class Person {
         if(age >= 18) return true;
           else return false;
     }
-
 }
